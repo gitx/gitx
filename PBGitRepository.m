78955b5717b3bd9a639033a2e40903a44707e116
apla <dot.and.thing@gmail.com>
2011-03-30 00:59:49 +0400

    fix for push error when origin has mirror flag

--- PBGitRepository.m
+++ PBGitRepository.m
@@ -690,22 +690,34 @@ NSString* PBGitRepositoryErrorDomain = @"GitXErrorDomain";
 	}
 	NSString *remoteName = [remoteRef remoteName];
 	[arguments addObject:remoteName];
-
+	
 	NSString *branchName = nil;
-	if ([ref isRemote] || !ref) {
-		branchName = @"all updates";
-	}
-	else if ([ref isTag]) {
-		branchName = [NSString stringWithFormat:@"tag '%@'", [ref tagName]];
-		[arguments addObject:@"tag"];
-		[arguments addObject:[ref tagName]];
-	}
-	else {
-		branchName = [ref shortName];
-		[arguments addObject:branchName];
+	NSString *actionType = nil;
+
+	if ([config valueForKeyPath:[NSString stringWithFormat:@"remote.%@.mirror", remoteName]]) {
+		// we must check for mirror parameter in config.
+		// if we push branch name in this case to the arguments, push failed
+		actionType = @"Mirroring";
+	} else {
+		
+		if ([ref isRemote] || !ref) {
+			branchName = @"all updates";
+		}
+		else if ([ref isTag]) {
+			branchName = [NSString stringWithFormat:@"tag '%@'", [ref tagName]];
+			[arguments addObject:@"tag"];
+			[arguments addObject:[ref tagName]];
+		}
+		else {
+			branchName = [ref shortName];
+			[arguments addObject:branchName];
+		}
+
+		actionType = [NSString stringWithFormat:@"Pushing %@", branchName];
+		
 	}
 
-	NSString *description = [NSString stringWithFormat:@"Pushing %@ to %@", branchName, remoteName];
+	NSString *description = [actionType stringByAppendingFormat:@" to %@", remoteName];
 	NSString *title = @"Pushing to remote";
 	[PBRemoteProgressSheet beginRemoteProgressSheetForArguments:arguments title:title description:description inRepository:self];
 }
