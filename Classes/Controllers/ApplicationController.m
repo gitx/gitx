547781374640576ece1957a560af031698cce731
Andrei Fokau <andrei@5monkeys.se>
2017-02-09 11:22:17 +0000

    Update app name, urls, and copyright.

--- Classes/Controllers/ApplicationController.m
+++ Classes/Controllers/ApplicationController.m
@@ -164,10 +164,10 @@ static OpenRecentController *recentsDialog = nil;
 		[dict addEntriesFromDictionary:[[NSDictionary alloc] initWithObjectsAndKeys:gitversion, @"Version", nil]];
 
 #ifdef DEBUG_BUILD
-	[dict addEntriesFromDictionary:[[NSDictionary alloc] initWithObjectsAndKeys:@"GitX-dev (DEBUG)", @"ApplicationName", nil]];
+	[dict addEntriesFromDictionary:[[NSDictionary alloc] initWithObjectsAndKeys:@"GitX(N) (DEBUG)", @"ApplicationName", nil]];
 #endif
 
-	[dict addEntriesFromDictionary:[[NSDictionary alloc] initWithObjectsAndKeys:@"GitX-dev (rowanj fork)", @"ApplicationName", nil]];
+	[dict addEntriesFromDictionary:[[NSDictionary alloc] initWithObjectsAndKeys:@"GitX(N)", @"ApplicationName", nil]];
 
 	[NSApp orderFrontStandardAboutPanelWithOptions:dict];
 }
