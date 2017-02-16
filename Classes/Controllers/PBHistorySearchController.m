gitx/gitx

commit dddc3e9af2115d4f8ab78d2ef3177d912b6d1b9e

Fix warnings about incorrect integer types
Mostly by changing int to long/NSInteger/NSUInteger

Add long_to_integer_bound() function to explicitly
convert from long to the required field length in
PBGitGrapher.mm as discussed in #16.

Classes/Controllers/PBHistorySearchController.m
@@ -70,7 +70,7 @@
 
 - (void)selectSearchMode:(id)sender
 {
-	self.searchMode = [(NSView*)sender tag];
+	[self setSearchMode:PBSearchModeForInteger([(NSView*)sender tag])];
 	[self updateSearch:self];
 }
 
@@ -106,13 +106,13 @@
 
 - (IBAction)updateSearch:(id)sender
 {
-	if (self.searchMode == kGitXBasicSeachMode)
+	if (self.searchMode == PBHistorySearchModeBasic)
 		[self startBasicSearch];
 	else
 		[self startBackgroundSearch];
 }
 
-- (void)setHistorySearch:(NSString *)searchString mode:(NSInteger)mode
+- (void)setHistorySearch:(NSString *)searchString mode:(PBHistorySearchMode)mode
 {
 	if (searchString && ![searchString isEqualToString:@""]) {
 		self.searchMode = mode;
@@ -125,7 +125,7 @@
 - (void)awakeFromNib
 {
 	[self setupSearchMenuTemplate];
-	self.searchMode = [PBGitDefaults historySearchMode];
+	self.searchMode = PBSearchModeForInteger([PBGitDefaults historySearchMode]);
 
 	[self updateUI];
 
@@ -239,22 +239,22 @@
 
 	item = [[NSMenuItem alloc] initWithTitle:kGitXBasicSearchLabel action:@selector(selectSearchMode:) keyEquivalent:@""];
 	[item setTarget:self];
-    [item setTag:kGitXBasicSeachMode];
+    [item setTag:PBHistorySearchModeBasic];
     [searchMenu addItem:item];
 
 	item = [[NSMenuItem alloc] initWithTitle:kGitXPickaxeSearchLabel action:@selector(selectSearchMode:) keyEquivalent:@""];
 	[item setTarget:self];
-    [item setTag:kGitXPickaxeSearchMode];
+    [item setTag:PBHistorySearchModePickaxe];
     [searchMenu addItem:item];
 
 	item = [[NSMenuItem alloc] initWithTitle:kGitXRegexSearchLabel action:@selector(selectSearchMode:) keyEquivalent:@""];
 	[item setTarget:self];
-    [item setTag:kGitXRegexSearchMode];
+    [item setTag:PBHistorySearchModeRegex];
     [searchMenu addItem:item];
 
 	item = [[NSMenuItem alloc] initWithTitle:kGitXPathSearchLabel action:@selector(selectSearchMode:) keyEquivalent:@""];
 	[item setTarget:self];
-    [item setTag:kGitXPathSearchMode];
+    [item setTag:PBHistorySearchModePath];
     [searchMenu addItem:item];
 
     item = [NSMenuItem separatorItem];
@@ -289,35 +289,31 @@
 	if (!searchMenu)
 		return;
 
-	NSMenuItem *item;
-
-	item = [searchMenu itemWithTag:kGitXBasicSeachMode];
-	[item setState:(searchMode == kGitXBasicSeachMode) ? NSOnState : NSOffState];
-
-	item = [searchMenu itemWithTag:kGitXPickaxeSearchMode];
-	[item setState:(searchMode == kGitXPickaxeSearchMode) ? NSOnState : NSOffState];
-
-	item = [searchMenu itemWithTag:kGitXRegexSearchMode];
-	[item setState:(searchMode == kGitXRegexSearchMode) ? NSOnState : NSOffState];
-
-	item = [searchMenu itemWithTag:kGitXPathSearchMode];
-	[item setState:(searchMode == kGitXPathSearchMode) ? NSOnState : NSOffState];
+	[self updateSearchModeMenuItemWithTag:PBHistorySearchModeBasic inMenu:searchMenu];
+	[self updateSearchModeMenuItemWithTag:PBHistorySearchModePickaxe inMenu:searchMenu];
+	[self updateSearchModeMenuItemWithTag:PBHistorySearchModeRegex inMenu:searchMenu];
+	[self updateSearchModeMenuItemWithTag:PBHistorySearchModePath inMenu:searchMenu];
 
     [[searchField cell] setSearchMenuTemplate:searchMenu];
 
 	[PBGitDefaults setHistorySearchMode:searchMode];
 }
 
+- (void) updateSearchModeMenuItemWithTag:(PBHistorySearchMode)menuItemSearchMode inMenu:(NSMenu *) searchMenu {
+	NSMenuItem * menuItem = [searchMenu itemWithTag:menuItemSearchMode];
+	[menuItem setState:(searchMode == menuItemSearchMode) ? NSOnState : NSOffState];
+}
+
 - (void)updateSearchPlaceholderString
 {
 	switch (self.searchMode) {
-		case kGitXPickaxeSearchMode:
+		case PBHistorySearchModePickaxe:
 			[[searchField cell] setPlaceholderString:kGitXPickaxeSearchLabel];
 			break;
-		case kGitXRegexSearchMode:
+		case PBHistorySearchModeRegex:
 			[[searchField cell] setPlaceholderString:kGitXRegexSearchLabel];
 			break;
-		case kGitXPathSearchMode:
+		case PBHistorySearchModePath:
 			[[searchField cell] setPlaceholderString:kGitXPathSearchLabel];
 			break;
 		default:
@@ -333,11 +329,7 @@
 
 - (void)setSearchMode:(PBHistorySearchMode)mode
 {
-	if ((mode < kGitXBasicSeachMode) || (mode >= kGitXMaxSearchMode))
-		mode = kGitXBasicSeachMode;
-
-	searchMode = mode;
-	[PBGitDefaults setHistorySearchMode:searchMode];
+	[PBGitDefaults setHistorySearchMode:mode];
 
 	[self updateSearchMenuState];
 	[self updateSearchPlaceholderString];
@@ -414,12 +406,12 @@
 
 	NSMutableArray *searchArguments = [NSMutableArray arrayWithObjects:@"log", @"--pretty=format:%H", nil];
 	switch (self.searchMode) {
-		case kGitXRegexSearchMode:
+		case PBHistorySearchModeRegex:
 			[searchArguments addObject:@"--pickaxe-regex"];
-		case kGitXPickaxeSearchMode:
+		case PBHistorySearchModePickaxe:
 			[searchArguments addObject:[NSString stringWithFormat:@"-S%@", searchString]];
 			break;
-		case kGitXPathSearchMode:
+		case PBHistorySearchModePath:
 			[searchArguments addObject:@"--"];
 			[searchArguments addObjectsFromArray:[searchString componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]];
 			break;

Classes/Controllers/ApplicationController.m
@@ -62,7 +62,7 @@ static OpenRecentController* recentsDialog = nil;
 	[NSApp setServicesProvider:services];
 
 	// Force update the services menu if we have a new services version
-	int serviceVersion = [[NSUserDefaults standardUserDefaults] integerForKey:@"Services Version"];
+	NSInteger serviceVersion = [[NSUserDefaults standardUserDefaults] integerForKey:@"Services Version"];
 	if (serviceVersion < 2)
 	{
 		NSLog(@"Updating services menu…");

Classes/Controllers/OpenRecentController.m
@@ -101,8 +101,10 @@
 	}
 	else if(commandSelector == @selector(moveUp:)) {
 		if(selectedResult != nil) {
-			int index = [currentResults indexOfObject: selectedResult]-1;
-			if(index < 0) index = 0;
+			NSUInteger index = [currentResults indexOfObject:selectedResult] - 1;
+			if (index > 0) {
+				index -= 1;
+			}
 			selectedResult = [currentResults objectAtIndex:index];
 			[resultViewer selectRowIndexes:[NSIndexSet indexSetWithIndex:index] byExtendingSelection:FALSE];
 			[resultViewer scrollRowToVisible:index];
@@ -111,7 +113,7 @@
 	}
 	else if(commandSelector == @selector(moveDown:)) {
 		if(selectedResult != nil) {
-			int index = [currentResults indexOfObject: selectedResult]+1;
+			NSUInteger index = [currentResults indexOfObject:selectedResult] + 1;
 			if(index >= [currentResults count]) index = [currentResults count] - 1;
 			selectedResult = [currentResults objectAtIndex:index];
 			[resultViewer selectRowIndexes:[NSIndexSet indexSetWithIndex:index] byExtendingSelection:FALSE];
@@ -146,8 +148,8 @@
 }
 
 - (IBAction)changeSelection:(id) sender {
-	int i = [resultViewer selectedRow];
-	if(i >= 0 && i < [currentResults count])
+	NSInteger i = resultViewer.selectedRow;
+	if(i >= 0 && i < currentResults.count)
 		selectedResult = [currentResults objectAtIndex: i];
 	else 
 		selectedResult = nil;

Classes/Controllers/PBGitCommitController.m
@@ -336,7 +336,7 @@
 - (void)flagsChanged:(NSEvent *)theEvent
 {
     BOOL altDown = !!([theEvent modifierFlags] & NSAlternateKeyMask);
-    int currIndex = [controlsTabView indexOfTabViewItem:controlsTabView.selectedTabViewItem];
+    NSInteger currIndex = [controlsTabView indexOfTabViewItem:controlsTabView.selectedTabViewItem];
     int desiredIndex = altDown ? kControlsTabIndexStash : kControlsTabIndexCommit;
     if (currIndex != desiredIndex) {
         [controlsTabView selectTabViewItemAtIndex:desiredIndex];

Classes/Controllers/PBGitHistoryController.h
@@ -44,7 +44,7 @@
 
 	NSArray *currentFileBrowserSelectionPath;
     QLPreviewPanel *previewPanel;
-	int selectedCommitDetailsIndex;
+	NSInteger selectedCommitDetailsIndex;
 	BOOL forceSelectionUpdate;
 	PBGitTree *gitTree;
 	NSArray<PBGitCommit *> *webCommits;
@@ -56,7 +56,7 @@
 @property (readonly) PBRefController *refController;
 @property (readonly) PBHistorySearchController *searchController;
 
-@property (assign) int selectedCommitDetailsIndex;
+@property (assign) NSInteger selectedCommitDetailsIndex;
 @property PBGitTree* gitTree;
 @property NSArray<PBGitCommit *> *webCommits;
 @property NSArray<PBGitCommit *> *selectedCommits;

Classes/Controllers/PBGitHistoryController.m
@@ -206,7 +206,7 @@
 	return [self.selectedCommits isEqualToArray:[commitController selectedObjects]];
 }
 
-- (void) setSelectedCommitDetailsIndex:(int)detailsIndex
+- (void) setSelectedCommitDetailsIndex:(NSInteger)detailsIndex
 {
 	if (selectedCommitDetailsIndex == detailsIndex)
 		return;
@@ -217,7 +217,7 @@
 	[self updateKeys];
 }
 
-- (int) selectedCommitDetailsIndex
+- (NSInteger) selectedCommitDetailsIndex
 {
 	return selectedCommitDetailsIndex;
 }
@@ -582,7 +582,7 @@
 - (void)showCommitsFromTree:(id)sender
 {
 	NSString *searchString = [(NSArray *)[sender representedObject] componentsJoinedByString:@" "];
-	[searchController setHistorySearch:searchString mode:kGitXPathSearchMode];
+	[searchController setHistorySearch:searchString mode:PBHistorySearchModePath];
 }
 
 - (void) checkoutFiles:(id)sender

Classes/Controllers/PBGitRepositoryDocument.m
@@ -228,9 +228,9 @@ NSString *PBGitRepositoryDocumentType = @"Git Repository";
 	NSDictionary *arguments = [command arguments];
 	NSString *searchString = [arguments objectForKey:kGitXFindSearchStringKey];
 	if (searchString) {
-		NSInteger mode = [[arguments objectForKey:kGitXFindInModeKey] integerValue];
 		[PBGitDefaults setShowStageView:NO];
 		[self.windowController showHistoryView:self];
+		PBHistorySearchMode mode = PBSearchModeForInteger([[arguments objectForKey:kGitXFindInModeKey] integerValue]);
 		[self.windowController setHistorySearch:searchString mode:mode];
 	}
 }

Classes/Controllers/PBGitSidebarController.h
@@ -8,6 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 #import "PBViewController.h"
+#import "PBHistorySearchMode.h"
 
 @class PBSourceViewItem;
 @class PBGitHistoryController;
@@ -39,7 +40,7 @@
 
 - (IBAction) fetchPullPushAction:(id)sender;
 
-- (void)setHistorySearch:(NSString *)searchString mode:(NSInteger)mode;
+- (void)setHistorySearch:(NSString *)searchString mode:(PBHistorySearchMode)mode;
 
 @property(readonly) NSMutableArray *items;
 @property(readonly) PBSourceViewItem *remotes;

commit febdaa4ebf4c5fdcfa9dd1624d19f221e20bf976

Set the searchMode ivar in its setter again
Regressed in dddc3e9af2115d4f8ab78d2ef3177d912b6d1b9e

Fixes #37

Classes/Controllers/PBHistorySearchController.m
@@ -331,6 +331,7 @@
 
 - (void)setSearchMode:(PBHistorySearchMode)mode
 {
+	searchMode = mode;
 	[PBGitDefaults setHistorySearchMode:mode];
 
 	[self updateSearchMenuState];