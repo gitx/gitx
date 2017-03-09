779de887cc1d753aa36012dc0e4513c4481079c3
Max Langer <maxlanger@me.com>
2017-03-09 19:29:34 +0100

    Remove unused method from public interface
    the method remoteUrls is only used internally by the class

--- Classes/git/PBGitRepository.h
+++ Classes/git/PBGitRepository.h
@@ -131,7 +131,6 @@ typedef enum branchFilterTypes {
 - (BOOL) hasRemotes;
 - (PBGitRef *) remoteRefForBranch:(PBGitRef *)branch error:(NSError **)error;
 - (NSString *) infoForRemote:(NSString *)remoteName;
-- (NSDictionary *) remoteURLs;
 - (void) openWebsiteOfRemote:(NSString*)remoteName;
 - (NSString* _Nullable) remoteWebsiteNameForRemote:(NSString* _Nullable)remote;
 
