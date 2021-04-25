22829f4055c95b7de0e1647424ce1ff035a85e6a
Kent Sutherland <git@ksuther.com>
2021-04-25 17:21:34 +0000

    Remove instance variables that have a FIXME on them Reference the repository and windowController proeprties instead

--- Classes/Controllers/PBViewController.h
+++ Classes/Controllers/PBViewController.h
@@ -11,11 +11,7 @@
 #import "PBGitRepository.h"
 #import "PBGitWindowController.h"
 
-@interface PBViewController : NSViewController {
-	// FIXME: these ivars must go, but most controller out there access it directly, so, not today
-	__weak PBGitRepository *repository;
-	__weak PBGitWindowController *superController;
-}
+@interface PBViewController : NSViewController
 
 @property (nonatomic, weak, readonly) PBGitRepository *repository;
 @property (weak, readonly) PBGitWindowController *windowController;
