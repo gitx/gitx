f0c7bb3d713c94f36e41880bc4761baaca5f7646
Hannes Achleitner <hannes.software@gmx.at>
2026-09-13 10:07:43 +0200

    Add commit message to menu
    This avoids some misleading understanding of what is the concerning commit

--- Classes/Controllers/PBGitHistoryController.m
+++ Classes/Controllers/PBGitHistoryController.m
@@ -28,6 +28,7 @@
 #import "PBGitRevisionCell.h"
 #import "PBGitStash.h"
 #import "PBGitSidebarController.h"
+#import "NSString_Truncate.h"
 
 #define kHistorySelectedDetailIndexKey @"PBHistorySelectedDetailIndex"
 #define kHistoryDetailViewIndex 0
@@ -1234,7 +1235,9 @@
 	BOOL isHead = [firstCommit.OID isEqual:firstCommit.repository.headOID];
 
 	if (isSingleCommitSelection) {
-		[items addObject:[NSMenuItem pb_itemWithTitle:NSLocalizedString(@"Checkout Commit", @"Contextual Menu Item to check out the selected commit") action:@selector(checkout:) enabled:YES]];
+		NSString *truncatedSubject = [firstCommit.subject truncateToLength:13 mode:PBNSStringTruncateModeEnd indicator:@"..."];
+		NSString *checkoutCommitTitle = [NSString stringWithFormat:NSLocalizedString(@"Checkout Commit “%@”", @"Contextual Menu Item to check out the selected commit, %@ is the first part of its subject"), truncatedSubject];
+		[items addObject:[NSMenuItem pb_itemWithTitle:checkoutCommitTitle action:@selector(checkout:) enabled:YES]];
 		[items addObject:[NSMenuItem separatorItem]];
 
 		[items addObject:[NSMenuItem pb_itemWithTitle:NSLocalizedString(@"Create Branch…", @"Contextual Menu Item to create a branch at the selected commit") action:@selector(createBranch:) enabled:YES]];
